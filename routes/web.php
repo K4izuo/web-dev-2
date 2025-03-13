9662471c7ea16d8b79589cb1bd0d8faab6a89e24
K4izuo <crisjutineoracion22@gmail.com>
2025-03-13 22:17:23 +0800

    update: added a login form with authentication note kulang pa logout

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\StudentsController;
+use App\Http\Middleware\AuthContext;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/web', function () {
-    return view('layouts.StudentView');
+Route::get('/', function () {
+  return view('pages.admin_form');
 });
 
-//View all Students
-Route::get('/', [StudentsController::class, 'index'])->name('std.viewAll');
+//Auth
+Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth-login', [AuthController::class, 'userLogin'])->name('auth.userLogin');
 
-//Create New Students
-Route::post('/create-new', [StudentsController::class, 'createNewSTD'])->name('std.create');
-Route::put('/update/{id}', [StudentsController::class, 'updateSTD'])->name('std.update');
-Route::delete('/delete/{id}', [StudentsController::class, 'deleteSTD'])->name('std.delete');
+//Middleware
+Route::middleware([AuthContext::class])->group(function () {
+  Route::get('/students', [StudentsController::class, 'index'])->name('std.viewAll');
+  Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+  //Create New Students
+  Route::post('/create-new', [StudentsController::class, 'createNewSTD'])->name('std.create');
+  Route::put('/update/{id}', [StudentsController::class, 'updateSTD'])->name('std.update');
+  Route::delete('/delete/{id}', [StudentsController::class, 'deleteSTD'])->name('std.delete');
+});
